0416e8a4a04986036623718c296b7938f5112be5
danh <danh@jumei.com>
2018-03-20 17:08:37 +0800

    webpack配置

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // "extends": "airbnb",
+    "parserOptions": {
+        "ecmaVersion": 7,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+      }
+};
---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.scss';
+
+class App extends React.Component {
+    render() {
+      return (
+        <div className="container">
+          <h1 className="header">react app</h1>
+          <p>222233333</p>
+        </div>
+      );
+    }
+}
+
+export default App;
