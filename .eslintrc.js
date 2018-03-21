ac252f9f878c6ab9866990842a06c45b73564147
danh <danh@jumei.com>
2018-03-21 10:40:43 +0800

    配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,19 @@
 module.exports = {
-    // "extends": "airbnb",
-    "parserOptions": {
-        "ecmaVersion": 7,
+    parser: "babel-eslint", // 使用babel-eslint解析支持jsx
+    extends: ['standard', 'plugin:react/recommended'],
+    plugins: [
+        'react'
+    ],
+    parserOptions: {
+        "ecmaVersion": 7, // 支持ES7
         "sourceType": "module",
         "ecmaFeatures": {
             "jsx": true
         }
-      }
+    },
+    rules: { // 0,1,2 对应 off warninng error
+        'semi': [2, 'always', { 'omitLastInOneLineBlock': true }], // 句末必须加分号，{ 'omitLastInOneLineBlock': true }忽略花括号在同一行（内容也就在同一行了）的语句块中的最后一个分号
+        'space-before-function-paren': [2, 'never'], // function左括号之前禁用空格
+        'comma-dangle': [2, 'only-multiline'], // 拖尾逗号
+    }
 };
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './index.scss';
 
 class App extends React.Component {
-    render() {
-      return (
-        <div className="container">
-          <h1 className="header">react app</h1>
-          <p>222233333</p>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="container">
+        <h1 className="header">react app</h1>
+        <p>222233333</p>
+      </div>
+    );
+  }
 }
 
 export default App;
